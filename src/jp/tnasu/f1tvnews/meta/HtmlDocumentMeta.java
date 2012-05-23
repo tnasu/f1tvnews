e3c10a90ef22978cf7811b71cfb4416cb7e20cdb
toshimasa.nasu <toshimasa.nasu>
2012-05-23 17:48:05 +0900

    version2

--- src/jp/tnasu/f1tvnews/meta/HtmlDocumentMeta.java
+++ src/jp/tnasu/f1tvnews/meta/HtmlDocumentMeta.java
@@ -1,6 +1,6 @@
 package jp.tnasu.f1tvnews.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2012-05-23 15:29:51")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2012-05-23 17:44:56")
 /** */
 public final class HtmlDocumentMeta extends org.slim3.datastore.ModelMeta<jp.tnasu.f1tvnews.model.HtmlDocument> {
 
